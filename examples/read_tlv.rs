dfe9dcc3b022b44e5b8bb0b2824702ca66434ac7
Tobias Freese <wolfshund98@gmail.com>
2023-05-16 14:38:06 +0200

    Start implemeneting coversion to registration

--- examples/read_tlv.rs
+++ examples/read_tlv.rs
@@ -1,5 +1,6 @@
 use std::{collections::HashMap, fs};
 
+use card_reader::data::Registration;
 use color_eyre::Result;
 use iso7816_tlv::ber::{
     Tlv,
@@ -18,6 +19,8 @@ fn main() -> Result<()> {
     let full_reg = [reg_a, reg_b, reg_c].concat();
 
     let hash_map = tlv_to_hash_map(&full_reg);
+
+    let registration = hash_map_to_registration(&hash_map);
 
     println!("{hash_map:#?}");
 
@@ -47,3 +50,45 @@ fn add_values<'a>(tlv: &'a Tlv, hash_map: &mut HashMap<String, &'a Vec<u8>>, pre
         }
     }
 }
+
+fn hash_map_to_registration(hash_map: &HashMap<String, &Vec<u8>>) -> Registration {
+    Registration {
+        issuer_state: hash_map.string("71/9F33"),
+        issuer_authority: hash_map.string("71/9f35"),
+        document_number: (),
+        registration_number: (),
+        date_of_first_registration: (),
+        personal_data: (),
+        vehicle: (),
+        vehicle_identification_number: (),
+        mass: (),
+        vehicle_mass_with_body: (),
+        period_of_validity: (),
+        date_of_registration: (),
+        type_approval_number: (),
+        engine: (),
+        power_weight_ratio: (),
+        seating_capacity: (),
+        vechicle_category: (),
+        maximum_towable_mass: (),
+        colour: (),
+        maximum_speed: (),
+        exhaust_emissions: (),
+    }
+}
+
+trait HashMapToRegistrationHelper {
+    fn string(&self, path: &str) -> String;
+}
+
+impl HashMapToRegistrationHelper for HashMap<String, &Vec<u8>> {
+    fn string(&self, path: &str) -> String {
+        if let Some(bytes) = self.get(path) {
+            String::from_utf8_lossy(&bytes)
+        } else {
+            String::from("Not found")
+        }
+    }
+}
+
+
--- src/data.rs
+++ src/data.rs
@@ -1,69 +1,69 @@
-struct Registration {
+pub struct Registration {
     // From registration A
-    issuer_state: String,
-    issuer_authority: String,
-    document_number: String,
-    registration_number: String,
-    date_of_first_registration: String,
-    personal_data: PersonalData,
-    vehicle: Vehicle,
-    vehicle_identification_number: String,
-    mass: Mass,
-    vehicle_mass_with_body: String,
-    period_of_validity: String,
-    date_of_registration: String,
-    type_approval_number: String,
-    engine: Engine,
-    power_weight_ratio: String,
-    seating_capacity: SeatingCapacity,
+    pub issuer_state: String,
+    pub issuer_authority: String,
+    pub document_number: String,
+    pub registration_number: String,
+    pub date_of_first_registration: String,
+    pub personal_data: PersonalData,
+    pub vehicle: Vehicle,
+    pub vehicle_identification_number: String,
+    pub mass: Mass,
+    pub vehicle_mass_with_body: String,
+    pub period_of_validity: String,
+    pub date_of_registration: String,
+    pub type_approval_number: String,
+    pub engine: Engine,
+    pub power_weight_ratio: String,
+    pub seating_capacity: SeatingCapacity,
     // From registration B
-    vechicle_category: String,
-    maximum_towable_mass: MaximumTowableMass,
-    colour: String,
-    maximum_speed: String,
-    exhaust_emissions: ExhaustEmisions,
+    pub vechicle_category: String,
+    pub maximum_towable_mass: MaximumTowableMass,
+    pub colour: String,
+    pub maximum_speed: String,
+    pub exhaust_emissions: ExhaustEmisions,
 }
 
-struct PersonalData {
-    certificate_holder: CertificateHolder,
-    vehicles_owner: Option<bool>
+pub struct PersonalData {
+    pub certificate_holder: CertificateHolder,
+    pub vehicles_owner: Option<bool>
 }
 
-struct CertificateHolder {
-    surname_or_business_name: String,
-    other_name_or_initials: String,
-    address: String,
+pub struct CertificateHolder {
+    pub surname_or_business_name: String,
+    pub other_name_or_initials: String,
+    pub address: String,
 }
 
-struct Vehicle {
-    make: String,
-    vehicle_type: String,
-    commercial_descriptons: String,
+pub struct Vehicle {
+    pub make: String,
+    pub vehicle_type: String,
+    pub commercial_descriptons: String,
 }
 
-struct Mass {
+pub struct Mass {
     // Registration A
-    max_permissiable_mass: String,
+    pub max_permissiable_mass: String,
     // Registration B
-    max_whole_vehicle_permissable_mass: String,
+    pub max_whole_vehicle_permissable_mass: String,
 }
 
-struct Engine {
-    capacity: String,
-    max_net_power: String,
-    fuel_type: String,
+pub struct Engine {
+    pub capacity: String,
+    pub max_net_power: String,
+    pub fuel_type: String,
 }
 
-struct SeatingCapacity {
-    number_of_seats: String,
-    nunmber_of_standing_places: String,
+pub struct SeatingCapacity {
+    pub number_of_seats: String,
+    pub nunmber_of_standing_places: String,
 }
 
-struct MaximumTowableMass {
-    braked: String,
-    unbraked: String,
+pub struct MaximumTowableMass {
+    pub braked: String,
+    pub unbraked: String,
 }
 
-struct ExhaustEmisions {
-    environmental_category: String,
+pub struct ExhaustEmisions {
+    pub environmental_category: String,
 }
