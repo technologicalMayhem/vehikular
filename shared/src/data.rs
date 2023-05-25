a74fda5a61cfa85ad1e07dc318e05fc34d5d903e
Tobias Freese <wolfshund98@gmail.com>
2023-05-25 11:58:31 +0200

    WIP commit

--- shared/src/data.rs
+++ shared/src/data.rs
@@ -1,6 +1,6 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Registration {
     // From registration A
     pub issuer_state: String, // 9F33
@@ -28,34 +28,34 @@ pub struct Registration {
 }
 
 #[allow(clippy::module_name_repetitions)]
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct PersonalData {
     pub certificate_holder: CertificateHolder,
     pub vehicles_owner: VehicleOwner // 86
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum VehicleOwner {
     Yes,
     No,
     Unknown
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct CertificateHolder {
     pub surname_or_business_name: String, // 83
     pub other_name_or_initials: String, // 84
     pub address: String, // 85
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Vehicle {
     pub make: String, // 87
     pub vehicle_type: String, // 88
     pub commercial_descriptons: String, // 89
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Mass {
     // Registration A
     pub maximum_technically_permissible_laden_mass: String, // 8B
@@ -64,26 +64,26 @@ pub struct Mass {
     pub maximum_permissible_laden_mass_of_the_whole_vehicle_in_service: String, // 97
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Engine {
     pub capacity: String, // 90
     pub max_net_power: String, // 91
     pub fuel_type: String, // 92
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct SeatingCapacity {
     pub number_of_seats: String, // 94
     pub nunmber_of_standing_places: String, // 95
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct MaximumTowableMass {
     pub braked: String, // 9B
     pub unbraked: String, // 9C
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ExhaustEmisions {
     pub environmental_category: String, // 9F31
 }
--- web-app/src/main.rs
+++ web-app/src/main.rs
@@ -1,3 +1,63 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use rocket::{serde::json::Json, tokio::sync::Mutex, State, response::Responder};
+use shared::data::Registration;
+use thiserror::Error;
+
+#[macro_use]
+extern crate rocket;
+
+struct AppData {
+    registrations: Vec<Registration>,
+}
+
+impl AppData {
+    fn new() -> Self {
+        Self {
+            registrations: vec![],
+        }
+    }
+}
+
+#[get("/registration/<reg_num>")]
+async fn get_registration(
+    reg_num: &str,
+    state: &State<Mutex<AppData>>,
+) -> Option<Json<Registration>> {
+    let data = state.lock().await;
+    data.registrations
+        .iter()
+        .filter_map(|reg| {
+            if reg.registration_number == reg_num {
+                Some(Json(reg.clone()))
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<Json<Registration>>>()
+        .pop()
+}
+
+#[post("/registration", format = "application/json", data = "<registration>")]
+async fn post_registration(registration: Json<Registration>, state: &State<Mutex<AppData>>) -> Result<(), RegistrationError> {
+    let mut lock = state.lock().await;
+    if lock.registrations.iter().any(|reg| reg.registration_number == registration.registration_number) {
+        Err(RegistrationError::AlreadyExists)
+    } else {
+        lock.registrations.push(registration.0);
+        Ok(())
+    }
+}
+
+#[derive(Debug, Error)]
+enum RegistrationError {
+    #[error("A registration with that registration number already exists.")]
+    AlreadyExists
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .manage(Mutex::new(AppData::new()))
+        .mount("/", routes![get_registration, post_registration])
 }
