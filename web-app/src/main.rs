9e8fd80740e40af5ffa33c006c730bc65cd769b9
Tobias Freese <wolfshund98@gmail.com>
2023-06-02 14:29:16 +0200

    Add put method for overwriting

--- web-app/src/main.rs
+++ web-app/src/main.rs
@@ -48,7 +48,7 @@ async fn get_registration(
 async fn post_registration(
     registration: Json<Registration>,
     state: &State<Mutex<AppData>>,
-) -> Result<(), RegistrationError> {
+) -> Result<RegistrationResult, RegistrationError> {
     let mut lock = state.lock().await;
     if lock
         .registrations
@@ -58,25 +58,55 @@ async fn post_registration(
         Err(RegistrationError::AlreadyExists)
     } else {
         lock.registrations.push(registration.0);
-        Ok(())
+        Ok(RegistrationResult::NoContent)
     }
+}
+
+#[put("/registration", format = "application/json", data = "<registration>")]
+async fn put_registration(
+    registration: Json<Registration>,
+    state: &State<Mutex<AppData>>,
+) -> Result<RegistrationResult, RegistrationError> {
+    let mut lock = state.lock().await;
+    if let Some(index) = lock
+        .registrations
+        .iter()
+        .position(|reg| reg.registration_number == registration.registration_number)
+    {
+        lock.registrations[index] = registration.0;
+        Ok(RegistrationResult::NoContent)
+    } else {
+        Err(RegistrationError::DoesNotExist)
+    }
+}
+
+enum RegistrationResult {
+    NoContent
 }
 
 #[derive(Debug)]
 enum RegistrationError {
     AlreadyExists,
+    DoesNotExist,
 }
 
 impl ErrorResponder for RegistrationError {
-    fn response(&self) -> (Status, String) {
+    fn response(&self) -> (Status, &str) {
         match self {
-            RegistrationError::AlreadyExists => (Status::Conflict, "A registration with that registration number already exists.".to_string()),
+            RegistrationError::AlreadyExists => (
+                Status::Conflict,
+                "A registration with that registration number already exists.",
+            ),
+            RegistrationError::DoesNotExist => (
+                Status::BadRequest,
+                "Can not update as no registration exits",
+            ),
         }
     }
 }
 
 trait ErrorResponder {
-    fn response(&self) -> (Status, String);
+    fn response(&self) -> (Status, &str);
 }
 
 #[rocket::async_trait]
@@ -86,14 +116,26 @@ impl<'r> Responder<'r, 'static> for RegistrationError {
         Response::build()
             .status(status)
             .header(ContentType::Plain)
-            .sized_body(body.len(), Cursor::new(body))
+            .sized_body(body.len(), Cursor::new(String::from(body)))
             .ok()
+    }
+}
+
+impl<'r> Responder<'r, 'static> for RegistrationResult {
+    fn respond_to(self, _: &'r Request<'_>) -> response::Result<'static> {
+        match self {
+            RegistrationResult::NoContent => Response::build()
+                .status(Status::NoContent)
+                .header(ContentType::Plain)
+                .ok()
+        }
     }
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .manage(Mutex::new(AppData::new()))
-        .mount("/", routes![get_registration, post_registration])
+    rocket::build().manage(Mutex::new(AppData::new())).mount(
+        "/",
+        routes![get_registration, post_registration, put_registration],
+    )
 }
