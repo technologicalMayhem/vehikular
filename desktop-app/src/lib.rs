081a99e3e23d38282546680f289bdacf871e80f3
Tobias Freese <wolfshund98@gmail.com>
2023-06-13 14:26:12 +0200

    Begin writing documentation and reformatting

--- desktop-app/src/lib.rs
+++ desktop-app/src/lib.rs
@@ -16,6 +16,11 @@ mod parsing;
 
 pub static DEBUG: Mutex<bool> = Mutex::new(false);
 
+/// Read all regisration files from the card and combines their data into the [``Registration``] struct for easier use.
+///
+/// # Errors
+///
+/// This function will return an error if an error occured whilst reading the card.
 pub fn read_card(card: &Card) -> Result<Registration, CardReadingError> {
     if !is_evrc_card(card)? {
         Err(CardReadingError::NotAneVrc)?;
@@ -24,7 +29,6 @@ pub fn read_card(card: &Card) -> Result<Registration, CardReadingError> {
     let files = read_files(
         card,
         vec![
-            File::FSOd,
             File::RegistrationA,
             File::RegistrationB,
             File::RegistrationC,
@@ -46,23 +50,49 @@ pub fn read_card(card: &Card) -> Result<Registration, CardReadingError> {
     Ok(combine_registrations(&registrations))
 }
 
+/// The errors that can occur during the card reading process.
 #[derive(Debug, Error)]
 pub enum CardReadingError {
+    /// The card is not a eVRC card.
     #[error("The card is not a eVRC card.")]
     NotAneVrc,
-    #[error("Got unsuccessful response from card. Command: {0:?}. Response: {1:?}")]
+    /// Got an unsuccessful response from card. Contains the command sent and the response received.
+    #[error("Got an unsuccessful response from card. Command: {0:?}. Response: {1:?}")]
     UnsuccesfulResponseFromCard(Vec<u8>, Vec<u8>),
+    /// Failed to read a TLV. This is a wrapped [``iso7816_tlv::Error::TlvError``] from the [``iso7816_tlv``] crate.
     #[error("Could not read TLV")]
     TlvReadError(#[from] TlvError),
+    /// A error occured whilst reading from the card. This is a wrapped [``pcsc::Error``] from the [``pcsc``] crate.
+    #[error("A error occured whilst reading the card: {0}")]
+    CardReadingError(#[from] pcsc::Error),
+    /// Failed to read data from the FCP template. This is a wrapped [``FcpParseError``].
     #[error("Could not parse the FCP template for {0:?}")]
     FailedToReadFcp(File, FcpParseError),
 }
 
+/// Tests whether or not this a eVRC card.
+///
+/// # Errors
+///
+/// This function will return an error if an error occured whilst reading the card.
 fn is_evrc_card(card: &Card) -> Result<bool, CardReadingError> {
+    const SELECT_EVRC_APPLICATION: &[u8; 17] = &[
+        0x00, 0xA4, 0x04, 0x00, 0x0B, 0xA0, 0x00, 0x00, 0x04, 0x56, 0x45, 0x56, 0x52, 0x2D, 0x30,
+        0x31, 0x00,
+    ];
+    const SELECT_EVRC_APPLICATION_EXPECTED_RESPONSE: &[u8; 15] = &[
+        0x6F, 0x0D, 0x84, 0x0B, 0xA0, 0x00, 0x00, 0x04, 0x56, 0x45, 0x56, 0x52, 0x2D, 0x30, 0x31,
+    ];
+
     let response = run_apdu(card, &SELECT_EVRC_APPLICATION.to_vec())?;
     Ok(response == SELECT_EVRC_APPLICATION_EXPECTED_RESPONSE)
 }
 
+/// .
+///
+/// # Errors
+///
+/// This function will return an error if an error occured whilst reading the card.
 fn read_files(card: &Card, files: Vec<File>) -> Result<HashMap<File, Vec<u8>>, CardReadingError> {
     let mut map = HashMap::new();
 
@@ -74,19 +104,18 @@ fn read_files(card: &Card, files: Vec<File>) -> Result<HashMap<File, Vec<u8>>, C
     Ok(map)
 }
 
+/// Sends a application protocol data unit (APDU) to the smartcard and returns it's response.
+///
+/// # Errors
+///
+/// This function will return an error if an error occured whilst reading the card.
 fn run_apdu(card: &Card, apdu: &Vec<u8>) -> Result<Vec<u8>, CardReadingError> {
     if is_debug() {
         println!("Sending APDU: {apdu:?}");
     }
 
     let mut response_buf = [0; MAX_BUFFER_SIZE];
-    let response = match card.transmit(apdu, &mut response_buf) {
-        Ok(response) => response,
-        Err(err) => {
-            eprintln!("Failed to transmit APDU command to card: {err}");
-            std::process::exit(1);
-        }
-    };
+    let response = card.transmit(apdu, &mut response_buf)?;
 
     if is_sucessful(response) {
         // We lop off the response bytes if it's succesful as they are not needed.
@@ -99,10 +128,18 @@ fn run_apdu(card: &Card, apdu: &Vec<u8>) -> Result<Vec<u8>, CardReadingError> {
     }
 }
 
+/// Checks whether we are in a debug mode. This is mainly used to figure out if additional debug information should be printed to the console.
+///
+/// TODO: Debug info like this should be either completly removed from this library or implemented in a better and less hacky approach.
+///
+/// # Panics
+///
+/// Panics if a lock on the [``Debug``] value could not be acquired.
 fn is_debug() -> bool {
     *DEBUG.lock().expect("Could not read debug value")
 }
 
+/// Checks if a response from the eVRC card has [0x90, 0x00], which indicates successful processing, at the end.
 fn is_sucessful(response: &[u8]) -> bool {
     let len = response.len();
     if len <= 1 {
@@ -112,14 +149,7 @@ fn is_sucessful(response: &[u8]) -> bool {
     response[len - 2..len] == [0x90, 0x00]
 }
 
-pub const SELECT_EVRC_APPLICATION: &[u8; 17] = &[
-    0x00, 0xA4, 0x04, 0x00, 0x0B, 0xA0, 0x00, 0x00, 0x04, 0x56, 0x45, 0x56, 0x52, 0x2D, 0x30, 0x31,
-    0x00,
-];
-pub const SELECT_EVRC_APPLICATION_EXPECTED_RESPONSE: &[u8; 15] = &[
-    0x6F, 0x0D, 0x84, 0x0B, 0xA0, 0x00, 0x00, 0x04, 0x56, 0x45, 0x56, 0x52, 0x2D, 0x30, 0x31,
-];
-
+/// Contains functions and data related to selecting and reading files from the eVRC smartcard.
 mod select_file {
     use color_eyre::Result;
     use iso7816_tlv::ber::Tlv;
@@ -140,17 +170,23 @@ mod select_file {
     /// Expected length byte. How long we expect the response to be. 00 indicates that we expect any length.
     const LE: u8 = 0x00;
 
+    /// Represetns the different files that cam be read from the card.
     #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
     pub enum File {
+        /// Contains crytographic information that can be used to confirm the cards authenticity.
         FSOd,
+        /// Contains the bulk of the vehicle information stored on the chip.
         RegistrationA,
+        /// Contaisn some additional vehicles information, this seems to be a EU standard.
         RegistrationB,
+        /// Contains data extensions which might contain additional information dependant on the vehicle.
         RegistrationC,
     }
 
     impl File {
+        /// Returns a reference to the binary identifier of this [`File`].
         #[must_use]
-        pub fn get_indetifier(&self) -> &[u8; 2] {
+        pub fn binary_identifier(&self) -> &[u8; 2] {
             match self {
                 File::FSOd => &[0x00, 0x1D],
                 File::RegistrationA => &[0xD0, 0x01],
@@ -159,6 +195,7 @@ mod select_file {
             }
         }
 
+        /// Returns the corresponding file for the given identifier. Panics if the given byte sequence does not coreespond to a file type.
         #[must_use]
         pub fn from_identifier(data: [u8; 2]) -> Self {
             match data {
@@ -171,13 +208,18 @@ mod select_file {
         }
     }
 
+    /// Retrieves a file from the smartcard.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if an error occured whilst reading the card.
     pub fn retrieve_file(card: &Card, file: File) -> Result<Vec<u8>, CardReadingError> {
         let fcp = select_file(card, file)?;
         read_file(card, &fcp)
     }
 
     fn select_file(card: &Card, file: File) -> Result<FcpTemplate, CardReadingError> {
-        let file_id = file.get_indetifier();
+        let file_id = file.binary_identifier();
         let apdu = vec![CLA, INS, P1, P2, LC, file_id[0], file_id[1], LE];
         let response = run_apdu(card, &apdu)?;
         let (fcp, _) = Tlv::parse(&response);
@@ -185,6 +227,11 @@ mod select_file {
         FcpTemplate::try_from(fcp?).map_err(|err| CardReadingError::FailedToReadFcp(file, err))
     }
 
+    /// Reads out the contents of the file stored on the smart card for the given [``FcpTemplate``].
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if an error occured whilst reading the card.
     #[allow(clippy::cast_possible_truncation)]
     fn read_file(card: &Card, fcp: &FcpTemplate) -> Result<Vec<u8>, CardReadingError> {
         let mut data: Vec<u8> = Vec::new();
