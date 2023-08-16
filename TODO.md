8fe6891862753e171f2c2392123713224a50c025
Tobias Freese <wolfshund98@gmail.com>
2023-08-16 09:21:29 +0200

    Remove obsolete files

--- TODO.md
+++
@@ -1,6 +0,0 @@
-# Things to implement
-- Landing page with list of cars
-- Reigster form
-- Login form
-- Profile information
-- The whole thing with the session token
--- update_entities.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sea-orm-cli generate entity --with-serde serialize --database-url 'postgres://vehikular:vehikular@localhost:5432/vehikular' -o web-app/src/database/entities
